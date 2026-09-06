2b54a4e842ef5b0719922f2d31b8906d49384a6f
RAJ <pankajgt2000@gmail.com>
2026-09-06 16:45:30 +0530

    again bug fix

--- employeeManagement/src/main.jsx
+++ employeeManagement/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import DataProvider from '../src/Data/DataProvider.jsx'
+import DataProvider from '../src/data/DataProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
     <DataProvider > 
